54d36cd159e96143a2cca298a9becb03a9417929
Kalyani Bharadwaj <kalyanibharadwaj@Kalyanis-Laptop.local>
2026-05-27 10:37:18 +0530

    updated homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,18 @@
 export default function Home() {
   return (
-    <main className="p-10">
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Kalyani Bharadwaj</h1>
+      <p>Welcome to my personal blog 🚀</p>
 
-      <h1 className="text-5xl font-bold">
-        ~ Kalyani's Blog ~
-      </h1>
-
-      <p className="mt-4">
-        I am a CS student at IIIT Pune and this is my blog to record all the things I learn, When I am not studying you can find me playing sports or making art.
+      <h2>About Me</h2>
+      <p>
+        I am learning Next.js, Java, networking, and computer science.
       </p>
 
-      <h2 className="mt-8 text-2xl">
-       Currently working on ~ 
-      </h2>
-
-      <ul className="mt-4 space-y-2">
-        <li>CS231 - stanford's computer vision course </li>
-        <li>Making projects related to opencv and python</li>
-        <li>leetcoding </li>
-        <li>learn something new</li>
-      </ul>
-
+      <h2>My First Blog Post</h2>
+      <p>
+        This website is hosted using GitHub Pages.
+      </p>
     </main>
-  )
+  );
 }
