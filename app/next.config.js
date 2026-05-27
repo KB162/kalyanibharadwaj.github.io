83852871fdc79d2d26d54b197d7fc90edcc99f42
Kalyani Bharadwaj <kalyanibharadwaj@Kalyanis-Laptop.local>
2026-05-27 10:47:32 +0530

    fixed github pages

---
+++ app/next.config.js
@@ -0,0 +1,6 @@
+const nextConfig = {
+  output: "export",
+  basePath: "/kalyanibharadwaj.github.io",
+};
+
+module.exports = nextConfig;
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,8 @@
 export default function Home() {
   return (
-    <main style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Kalyani Bharadwaj</h1>
-      <p>Welcome to my personal blog 🚀</p>
-
-      <h2>About Me</h2>
-      <p>
-        I am learning Next.js, Java, networking, and computer science.
-      </p>
-
-      <h2>My First Blog Post</h2>
-      <p>
-        This website is hosted using GitHub Pages.
-      </p>
+    <main style={{ padding: "40px" }}>
+      <h1>Hello Kalyani 🚀</h1>
+      <p>This is my custom website.</p>
     </main>
   );
 }
