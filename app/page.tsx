d72e2ca5b39d8d085110264ae1286591859a05e9
Kalyani Bharadwaj <kalyanibharadwaj@Kalyanis-Laptop.local>
2026-05-28 18:17:32 +0530

    updated website

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 export default function Home() {
   return (
-    <main style={{ padding: "40px" }}>
-      <h1>Hello Kalyani 🚀</h1>
-      <p>This is my custom website.</p>
+    <main>
+      <h1>Kalyani's Blog</h1>
+
+      <h2>About Me</h2>
+      <p>I like coding and electronics.</p>
+
+      <h2>Notes</h2>
+      <p>cs231 - </p>
+
+      <h2>Others</h2>
+      <p>Projects and ideas</p>
     </main>
-  );
+  )
 }
--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/kalyanibharadwaj.github.io', // only if repo name isn't username.github.io
-};
-export default nextConfig;
+}
+
+module.exports = nextConfig
