a08e979ff609a9bb35cdad13465c4c9efa9b68d9
KB162 <kalyanibharadwaj205@gmail.com>
2026-06-08 09:21:57 +0530

    Update page.tsx
    added first blog post

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,22 @@ export default function Home() {
       <h1>Kalyani's Blog</h1>
 
       <h2>About Me</h2>
-      <p>I like coding and electronics.</p>
+      <p>This blog is me recording my journey.</p>
+
+      <h2>Latest Post</h2>
+
+      <h3>learning computer vision</h3>
+      <p>
+        i made a small project that  worked on a concept of computer vision project using OpenCV and Python.
+        I learned how object detection works and how to process webcam input.
+      </p>
 
       <h2>Notes</h2>
-      <p>cs231 - </p>
+      <p>CS231</p>
+      <>electronics</>
 
       <h2>Others</h2>
       <p>Projects and ideas</p>
     </main>
-  )
+  );
 }
