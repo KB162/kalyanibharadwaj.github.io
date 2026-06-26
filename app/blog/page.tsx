01f5151b55a637ca95527bae5d621227dcbe3add
Kalyani Bharadwaj <kalyanibharadwaj@Kalyanis-Laptop.local>
2026-06-26 20:11:38 +0530

    Updated homepage

---
+++ app/blog/page.tsx
@@ -0,0 +1,19 @@
+export default function Blog() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>My Blog</h1>
+
+      <p>Welcome to my engineering notebook.</p>
+
+      <ul>
+        <li>
+          Building a Gesture Controlled LED
+        </li>
+
+        <li>
+          My CS231 Journey
+        </li>
+      </ul>
+    </main>
+  );
+}
