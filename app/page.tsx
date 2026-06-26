44c596300d3470ba869ce4c2e2f5cec16e6f9edb
Kalyani Bharadwaj <kalyanibharadwaj@Kalyanis-Laptop.local>
2026-06-26 19:54:55 +0530

    Updated hompage

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         Computer Vision • Robotics • AI</h2>
 
       <p>
-        I am a Computer Science Engineering student passionate about building
+        I am a Computer Science Engineering student at IIIT Pune , passionate about building
         intelligent systems using Computer Vision and embedded electronics.
       </p>
 
@@ -16,9 +16,10 @@ export default function Home() {
       <h2>Projects</h2>
 
       <ul>
-        <li>Gesture Controlled LED</li>
+        <li>Gesture Controlled LED using computer vision</li>
         <li>Hand Detection with MediaPipe</li>
         <li>CS231 Computer Vision Projects</li>
+        <li>Researching on paper related to MOS sensors and Neural Networks being used in detection of gases with a professor at IIIT Pune </li>
       </ul>
 
       <hr />
@@ -26,9 +27,20 @@ export default function Home() {
       <h2>Currently Learning</h2>
 
       <ul>
-        <li>Computer Vision</li>
-        <li>Large Language Models</li>
-        <li>Robotics</li>
+        <li>Computer Vision -
+            I started learning computer vision from the online course of stanford - CS231n 
+            Summary of each class in my own words - 
+
+
+        </li>
+        <li>Research - </li>
+        <li>I currently am involved in a research paper which is basically on MOS sensors and different Neural Networks being used to detect the compostion of the gased by applying the computer vision knowledge I learnt from CS231n program of stanford </li>
+
+        <li>Robotics -
+          I have recently built an interest in robotics and since I am a CS student I am learning it by making small electronics projects and integrating the computer vision knowledege from cs231 in it 
+          1.Gesture controlled LED 
+
+        </li>
       </ul>
     </main>
   );
